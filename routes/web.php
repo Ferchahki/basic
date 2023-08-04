86724b154e6fd472c2eb6af1a1a02a565433aeee
Ferchahki <alawiabdo000@gmail.com>
2023-08-04 21:16:57 +0100

    backend Dasborad

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,13 @@ Controller Resources MultiPicture
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
 
 
-    Route::get('/dashboard', function () {$users=User::all();return view('dashboard',compact('users'));})->name('dashboard');
+    Route::get('/dashboard', function () {
+
+        //$users=User::all();
+        //return view('dashboard',compact('users'));
+        return view('admin.index');
+
+    })->name('dashboard');
 
 
 });
